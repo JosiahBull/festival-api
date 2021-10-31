c339c5749b6a332ea4ffba21ae5fd2905199dbf2
JosiahBull <josiah.bull7@gmail.com>
2021-10-31 23:33:40 +1300

    feat: Created boilerplate for inital endpoints.

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use rocket::fs::NamedFile;
+
+#[macro_use] extern crate rocket;
+
+const API_NAME: &str = "Jo";
+
+#[get("/")]
+fn index() -> String {
+    format!("Welcome to {}'s API for converting text into downloadable wav files! Please make a request to /docs for documentation.", API_NAME)
+}
+
+#[get("/docs")]
+fn docs() -> NamedFile {
+    todo!()
+}
+
+#[get("/login")]
+fn login() -> String {
+    todo!()
+}
+
+#[get("/create")]
+fn create() -> String {
+    todo!()
+}
+
+#[get("/convert")]
+fn convert() -> String {
+    todo!()
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index, docs])
+        .mount("/api/v1/", routes![login, create, convert])
+}
